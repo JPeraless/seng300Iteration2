0f3156fc9536e55a0a9fc10864dc8b1dbb943c26
Caleb Cavilla <caleb.cavilla@gmail.com>
2023-03-19 00:22:05 -0600

    Changed amount due to positive

--- com.autovend.software/src/com/autovend/software/PayWithCashController.java
+++ com.autovend.software/src/com/autovend/software/PayWithCashController.java
@@ -92,7 +92,7 @@ public class PayWithCashController extends System implements BillSlotObserver, B
 	 */
 	public void deliverChange(double amountDue)  {
 		out.println("before: " + amountDue);
-		amountDue =  Math.ceil(amountDue/5) * 5; // round up to the nearest multiple of 5 to avoid under paying customer
+		amountDue = (int) Math.ceil((-1*amountDue)/5) * 5; // round up to the nearest multiple of 5 to avoid under paying customer
 		out.println("after: " + amountDue);
 		List<Integer> keyList = new ArrayList<Integer>(station.billDispensers.keySet());
 		Collections.reverse(keyList);
