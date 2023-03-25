ae7abc3b8b28556411d5338b075bd316cf9fc036
desobob11 <desmond.obrien@ucalgary.ca>
2023-03-25 12:17:35 -0600

    weight discrepancy skeleton

---
+++ com.autovend.software_0.2/src/WeightDiscrepancy.java
@@ -0,0 +1,43 @@
+import com.autovend.Barcode;
+
+public class WeightDiscrepancy {
+
+
+	public static void weightDiscrepancy() {
+		WeightDiscrepancy.attendantSignal();
+		WeightDiscrepancy.baggingAreaDiscrepancy();
+		WeightDiscrepancy.doNotBagRequest();
+	}
+	
+	
+	
+	//Option a
+	
+	public static void baggingAreaDiscrepancy() {
+		
+	}
+	
+	
+	//Option b
+	public static void doNotBagRequest() {
+		
+	}
+	
+	//Option c
+	
+	public static void attendantSignal() {
+		
+		
+		
+	}
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+}
