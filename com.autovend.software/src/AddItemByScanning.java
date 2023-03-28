6701a6e7cba7859d63b3c9c1f681f23842076d29
desobob11 <desmond.obrien@ucalgary.ca>
2023-03-27 19:57:23 -0600

    stuff from payemtn

---
+++ com.autovend.software/src/AddItemByScanning.java
@@ -0,0 +1,45 @@
+import com.autovend.Barcode;
+import com.autovend.devices.AbstractDevice;
+import com.autovend.devices.BarcodeScanner;
+import com.autovend.devices.SelfCheckoutStation;
+import com.autovend.devices.SimulationException;
+import com.autovend.devices.observers.AbstractDeviceObserver;
+import com.autovend.devices.observers.BarcodeScannerObserver;
+import com.autovend.products.BarcodedProduct;
+
+
+/**
+ * Permits customer to scan a barcode of an item to add it to their bill for
+purchase.
+ */
+
+
+public class AddItemByScanning extends System implements BarcodeScannerObserver{
+
+	private BarcodedProduct barcodedProduct;
+
+
+	public AddItemByScanning(BarcodedProduct currentBarcodedProduct, SelfCheckoutStation station){
+		super(station);
+		this.barcodedProduct = currentBarcodedProduct;
+
+	}
+
+	@Override
+	public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// Reacts to scanner and adds item which is detected
+	public void reactToBarcodeScannedEvent(BarcodeScanner barcodeScanner, Barcode barcode) throws SimulationException{
+		super.addBillList(barcodedProduct);
+	}
+
+}
