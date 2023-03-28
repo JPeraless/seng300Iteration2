90b963cf98b125b35b8c3dac1993165f7c9191d0
matt200500 <124231840+matt200500@users.noreply.github.com>
2023-03-27 23:06:52 -0600

    removed customer io from adding bags

--- com.autovend.software/src/AddOwnBags.java
+++ com.autovend.software/src/AddOwnBags.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+
 import com.autovend.Barcode;
 import com.autovend.BarcodedUnit;
+import com.autovend.SellableUnit;
 import com.autovend.devices.AbstractDevice;
 import com.autovend.devices.ElectronicScale;
 import com.autovend.devices.OverloadException;
@@ -8,42 +11,80 @@ import com.autovend.devices.SimulationException;
 import com.autovend.devices.observers.AbstractDeviceObserver;
 import com.autovend.devices.observers.ElectronicScaleObserver;
 
-
-
-
 public class AddOwnBags implements ElectronicScaleObserver {
-	
+	boolean attendantApprovedBags;
+		
 	/**
-	 * Method implementing Add Own Bags use case
-	 * 
-	 * Returns weight of bags, once they've been added, to calling code (System)
-	 * 
 	 * @param station - SelfCheckoutStation in use
-	 * @param io	- Customer IO interface
-	 * @return - 	Weight of bags
+	 * @param bags - Total number of bags customer wants to add
+	 * @return - Weight of bags
 	 * @throws OverloadException - in case added weight of bags exceeds scale's limit
+	 * 
 	 */
-	public double addOwnBags(SelfCheckoutStation station, CustomerIO io) throws OverloadException {
-		// assumed that customer has signalled that they want to add bags
-		// assumaed that system has signalled that customer should add own bags
+	
+	public void addOwnBags(SelfCheckoutStation station, int bags) throws OverloadException {
 		
+		this.attendantApprovedBags = attendantApprovedBags;
 		
-		// record scale's weight before bags were added
-		double initialWeight = station.baggingArea.getCurrentWeight();
-		
-		
-		// allow customer to add as many bags as they would like
-		// TODO: Deal with exception here? Custom exception message? idk
-		boolean done = false;
-		while (!done) {
-			double bagWeight = io.addBag(station.baggingArea);
-			this.reactToWeightChangedEvent(station.baggingArea , bagWeight);
-			done = io.doneAddingBags();
+		// assumed that customer was signaled that they want to add bags
+		System.out.println("Please add your bags");
+	
+		double bagWeight = 0;
+		for(int i = 0; i < bags; i ++) {
+			double weight = addBag(station.baggingArea);
+			bagWeight = bagWeight + weight;
 		}
 		
-		// return weight of bags once all bags have been added
-		return station.baggingArea.getCurrentWeight() - initialWeight;
+		// Idk if this counts as signaling system.
+		System.out.println("Weight has been changed, weight of bags is: " + bagWeight);
+		stationDisable(station);
+		
+		// Signaling attendant to approve of bags
+		System.out.println("Attendant called to approve of bags");
+		
+		// If attendant does not approve of bags
+		if (!attendantApprovedBags) {
+			throw new SimulationException("Bags not approved");
+		}
+		System.out.println("Bags approved");
+		
+		stationEnable(station); // enables station if bags are approved
+		System.out.println("Customer may continue with transaction");
 	}
+	
+	/*
+	 * Function to simulate a user adding bags to a scale
+	 * 
+	 * Weight of bags is a random weight in range (0, 1)
+	 * 
+	 */
+	public double addBag(ElectronicScale baggingArea) {
+		double weight = new Random().nextDouble();
+		SellableUnit bag = new BarcodedUnit(null, weight);
+		baggingArea.add(bag);
+		return weight;
+	}
+	
+	/** 
+	 * disables parts of SelfCheckoutStation that customer can interact with while adding bags
+	 * @param station - station to be disabled
+	 */
+	public static void stationDisable(SelfCheckoutStation station) {
+		station.handheldScanner.disable();
+		station.billInput.disable();
+		station.mainScanner.disable();
+	}
+	
+	/** 
+	 * enables parts of SelfCheckoutStation that customer can interact with
+	 * @param station - station to be enabled
+	 */
+	public static void stationEnable(SelfCheckoutStation station) {
+		station.handheldScanner.enable();
+		station.billInput.enable();
+		station.mainScanner.enable();
+	}
+	
 
 	@Override
 	public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
@@ -77,10 +118,6 @@ public class AddOwnBags implements ElectronicScaleObserver {
 	public void reactToOutOfOverloadEvent(ElectronicScale scale) {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	
-	
-	
+	}	
 	
 }
