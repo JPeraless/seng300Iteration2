f3b3806af53026f45ea78b058c460456e6aa99ad
victor0campos <victor.campos@ucalgary.ca>
2023-03-28 18:35:30 -0600

    Created a stub for bag dispenser and a constructor for Purchased bags

--- com.autovend.software/src/PurchaseBags.java
+++ com.autovend.software/src/PurchaseBags.java
@@ -1,5 +1,40 @@
+import com.autovend.Barcode;
+import com.autovend.BarcodedUnit;
+import com.autovend.Numeral;
+import com.autovend.devices.SelfCheckoutStation;
+
 
 public class PurchaseBags {
 
-	//where we will do the purchase bags
+public static final Barcode PURCHASEDBAGBARCODE = new Barcode(new Numeral[] {Numeral.six,Numeral.nine});
+private int numOfBags; 
+private SelfCheckoutStation station; 
+	
+
+
+//Constructor for Purchased bags 
+	public PurchaseBags(SelfCheckoutStation station, int numOfBags) {
+		this.station = station; 
+		this.numOfBags = numOfBags; 
+		
+				
+	}
+	
+	
+	
+	
+	
+
+// Stub for bag dispenser
+private BarcodedUnit BagDispenser(){
+	BarcodedUnit bag = new BarcodedUnit(PURCHASEDBAGBARCODE, 1f);
+	return bag; 
+		
+	}
+	
+
+
+
+
+
 }
