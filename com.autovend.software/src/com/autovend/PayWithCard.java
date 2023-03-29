dbd53eeab1a828545cb9f16657ec35a90895283c
Omar Tejada <tejada.omar03@gmail.com>
2023-03-29 03:16:57 -0600

    Extract Pay with Credit into generic Pay use case
    Abstraction to also be used with Pay with Cash in System

--- com.autovend.software/src/com/autovend/PayWithCard.java
+++ com.autovend.software/src/com/autovend/PayWithCard.java
@@ -1,76 +1,28 @@
 package com.autovend;
 
 import com.autovend.devices.*;
-import com.autovend.devices.observers.AbstractDeviceObserver;
-import com.autovend.devices.observers.CardReaderObserver;
-
 
 /**
  * PayWithCard as credit and debit need literally the same things and their
  * classes have the same parameters
  */
-public class PayWithCard extends AbstractDevice<PayWithCardObserver> {
-
-
-    protected SelfCheckoutStation checkoutStation;
-
-    /**
-     * Amount to be paid by the customer
-     */
-    protected int totalPayment;
+public class PayWithCard extends Pay<PayWithCardObserver> {
+    public PayWithCard(SelfCheckoutStation checkoutStation, PayWithCardObserver controller) {
+        super(checkoutStation, controller);
+    }
 
     /**
-     * Amount that has been paid by the customer
+     * Make a payment for paymentMade amount, and update toBePaid and amountPaid
+     * accordingly
      */
-    protected int amountPaid;
+    @Override
+    public void makePayment(double paymentMade) {
+        reduceAmountDue(paymentMade);
 
-
-    /**
-     * Class constructor
-     *
-     * @param checkoutStation
-     */
-    public PayWithCard(SelfCheckoutStation checkoutStation) {
-        if (checkoutStation == null) {
-            throw new SimulationException("Station cannot not be null");
+        if (getAmountPaid() > getTotalAmountDue()) {
+            // for (CardReaderObserver observer : observers) {
+            // observer.reactToCompletePaymentEvent(this);
+            // }
         }
-
-        this.checkoutStation = checkoutStation;
-
-
     }
-
-
-    /**
-     * Make a payment for paymentMade amount, and update toBePaid and amountPaid accordingly
-     */
-    public void makePayment(int paymentMade) {
-        amountPaid += paymentMade;
-
-        if (amountPaid >= totalPayment) {
-            for (CardReaderObserver observer : observers) {
-                // observer.reactToCompletePaymentEvent(this);
-            }
-        }
-
-    }
-
-
-    /**
-     *
-     * @return amount missing to be paid by customer
-     */
-    public int getToBePaid() {
-        return totalPayment - amountPaid;
-    }
-
-
-    /**
-     *
-     * @return amount paid so far by customer
-     */
-    public int getAmountPaid() {
-        return amountPaid;
-    }
-
 }
