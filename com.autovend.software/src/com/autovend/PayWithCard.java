5d27b7fa7325bcd67a82cebf88e744d8e23d4e00
tanvirahamed2001 <127472391+tanvirahamed2001@users.noreply.github.com>
2023-03-30 02:15:40 -0600

    Update PayWithCard.java
    pay with credit card

--- com.autovend.software/src/com/autovend/PayWithCard.java
+++ com.autovend.software/src/com/autovend/PayWithCard.java
@@ -2,21 +2,56 @@ package com.autovend;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Scanner;
 
+import com.autovend.Card.CardData;
 import com.autovend.devices.*;
+import com.autovend.external.CardIssuer;
+import com.autovend.software.CustomerIO;
 
 /**
  * PayWithCard as credit and debit need literally the same things and their
  * classes have the same parameters
  */
 public class PayWithCard extends Pay<PayWithCardObserver> {
-    public PayWithCard(SelfCheckoutStation checkoutStation, PayWithCardObserver controller) {
+    CardReader reader;
+    Card card;
+    String pin;
+    
+    CardIssuer bank;
+    Calendar expiry;
+    
+    BigDecimal amountPaid;
+    BigDecimal totalBill;
+    CustomerIO customer;
+    
+    boolean totalBillPaid = false;
+    
+    ArrayList<PayWithCardObserver> observers = new ArrayList<PayWithCardObserver>();
+    
+    
+
+    public PayWithCard(SelfCheckoutStation checkoutStation, PayWithCardObserver controller, Card cd, CustomerIO cus) {
         super(checkoutStation, controller);
+        reader = new CardReader();
+        card = cd;
+        
+        customer = cus;
+    }
+    
+    public void setBank(CardIssuer bk) {
+    	bank = bk;
+    }
+    
+    public void setExpiry(Calendar date) {
+    	expiry = date;
     }
 
     
-    CardReader reader = new CardReader();
+    
     /**
      * Make a payment for paymentMade amount, and update toBePaid and amountPaid
      * accordingly
@@ -32,20 +67,54 @@ public class PayWithCard extends Pay<PayWithCardObserver> {
         }
     }
     
-    public void payWithCard(Card card, double amount) throws IOException {
+    public void payWithCard(Card card, double amount, String type) throws IOException {
     	
-    	
-    	Scanner scan = new Scanner(System.in);
+    	reader.enable();
     	BufferedImage image = new BufferedImage(null, null, false, null);
-    	String pin = scan.nextLine();
+    	CardData data;
     	
-    	reader.swipe(card, image);
+    	if (type.equals("insert")) {
+        	data = reader.insert(card,  pin);
+    	} else if (type.equals("tap")) {
+        	data = reader.tap(card);
+    	} else {
+        	data = reader.swipe(card, image);
+    	}
     	
+    	bank.addCardData(data.getNumber(), data.getCardholder(), expiry, data.getCVV(), amountPaid);
+    	int holdNumber = bank.authorizeHold(data.getNumber(), amountPaid);
+    	//something to do with system. see usecase scenario number 7 and 8
     	
-    	reader.insert(card, pin);
+    	boolean transactionComplete = bank.postTransaction(data.getNumber(), holdNumber, amountPaid);
     	
-    	System.out.println("PIN: " + pin);
+    	if (!transactionComplete) {
+    		for (PayWithCardObserver observer : observers) {
+        		observer.reactToNotEnouighFunds();
+    		}
+    	} else {
+        	boolean holdreleased = bank.releaseHold(data.getNumber(), holdNumber);
+        	
+        	totalBill = totalBill.subtract(amountPaid);
+    	}
     	
-    	reader.tap(card);
+    	if (reader.remove()) {
+    		customer.paymentSessionComplete();
+    	}
+    	
+    	int res = totalBill.compareTo(new BigDecimal("0")); 
+    	
+    	if (res == 1) {
+    		totalBillPaid = false;
+    	} else if (res == 2) {
+    		totalBillPaid = true;
+    		//requires change
+    	} else {
+    		totalBillPaid = true;
+    	}
+    	
+    }
+    
+    public void setPin(String num) {
+    	pin = num;
     }
 }
