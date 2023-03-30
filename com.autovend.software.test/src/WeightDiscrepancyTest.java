09fc0a33eb30ca1c7f7696d0fa0605108d0a681d
anna-jeongah <100656227+anna-jeongah@users.noreply.github.com>
2023-03-30 14:42:47 -0600

    Update WeightDiscrepancyTest.java
    Tests for weight discrepancy finished

--- com.autovend.software.test/src/WeightDiscrepancyTest.java
+++ com.autovend.software.test/src/WeightDiscrepancyTest.java
@@ -28,6 +28,8 @@ import com.autovend.devices.SelfCheckoutStation;
 import com.autovend.devices.SimulationException;
 import com.autovend.external.ProductDatabases;
 import com.autovend.products.BarcodedProduct;
+import com.autovend.PriceLookUpCode;
+import com.autovend.PriceLookUpCodedUnit;
 
 public class WeightDiscrepancyTest extends BaseTestCase {
 	
@@ -61,5 +63,90 @@ public class WeightDiscrepancyTest extends BaseTestCase {
 		
 		
 	}
-
+	
+	@Test
+	public void checkDiscrepancy_noWeightDiscrepancy_stationEnabled() throws OverloadException {
+		//barcoded unit with weight 50f
+		BarcodedUnit unit1 = new BarcodedUnit(new Barcode(new Numeral[] {Numeral.one}), 50f);
+		this.system.weightDiscrepency(50f); //create a weight discrepancy with expected weight equal to actual weight
+		this.station.baggingArea.add(unit1); //add item to bagging area
+		//barcoded unit with weight 30f
+		BarcodedUnit unit2 = new BarcodedUnit(new Barcode(new Numeral[] {Numeral.one}), 30f);
+		this.system.weightDiscrepency(80f); //create a weight discrepancy with expected weight equal to actual weight of unit 2 plus, unit 1 that was previously added
+		this.station.baggingArea.add(unit2); //add item to bagging area
+		
+		//check if station is enabled
+		assertEquals(false, station.handheldScanner.isDisabled());
+		assertEquals(false, station.mainScanner.isDisabled());
+		assertEquals(false, station.billInput.isDisabled());
+		
+		assertEquals(false, system.getDiscrepancyActive());
+	}
+	
+	@Test
+	public void checkDiscrepancy_weightDiscrepancyAfterRemovingItem_stationDisabled() throws OverloadException {
+		//STEP1: ITEM SCANNED AND PLACED IN BAGGING AREA
+		//barcoded unit with weight 34f
+		BarcodedUnit unit1 = new BarcodedUnit(new Barcode(new Numeral[] {Numeral.one}), 34f);
+		this.system.weightDiscrepency(34f); //create a weight discrepancy with expected weight equal to actual weight
+		this.station.baggingArea.add(unit1); //add item to bagging area
+		
+		//check if station is enabled
+		assertEquals(false, station.handheldScanner.isDisabled());
+		assertEquals(false, station.mainScanner.isDisabled());
+		assertEquals(false, station.billInput.isDisabled());
+		
+		assertEquals(false, system.getDiscrepancyActive());
+		
+		//STEP2: SAME ITEM SCANNED ABOVE REMOVED FROM BAGGING AREA
+		this.system.weightDiscrepency(34f); //since unit1 was scanned, the expected weight of the scale is the weight of unit1 
+		this.station.baggingArea.remove(unit1); //remove unit1 from the bagging area
+		
+		//check if station is disabled
+		assertEquals(true, station.handheldScanner.isDisabled());
+		assertEquals(true, station.mainScanner.isDisabled());
+		assertEquals(true, station.billInput.isDisabled());
+		
+		assertEquals(true, system.getDiscrepancyActive());
+	}
+	
+	@Test
+	public void weightDiscrepancyOptions_weightDiscrepancyTrue_AttendantApprovalTrue() throws OverloadException {
+		//barcoded unit with weight 15f
+		BarcodedUnit unit1 = new BarcodedUnit(new Barcode(new Numeral[] {Numeral.one}), 15f);
+		
+		this.system.weightDiscrepency(10f); //create a weight discrepancy with expected weight not equal to actual weight
+		this.system.setCustomerNoBag(false);
+		this.system.setAttendantApproval(true);
+		this.station.baggingArea.add(unit1);
+		
+		//check if station is enabled
+		assertEquals(false, station.handheldScanner.isDisabled());
+		assertEquals(false, station.mainScanner.isDisabled());
+		assertEquals(false, station.billInput.isDisabled());
+		
+		assertEquals(false, system.getUnApprovedDiscrepancy());
+		assertEquals(false, system.getDiscrepancyActive());
+	}
+	
+	@Test
+	public void weightDiscrepancyOptions_weightDiscrepancyTrue_CustomerNoBagTrue() throws OverloadException {
+		//barcoded unit with weight 15f
+		BarcodedUnit unit1 = new BarcodedUnit(new Barcode(new Numeral[] {Numeral.one}), 15f);
+		
+		this.system.weightDiscrepency(5f); //create a weight discrepancy with expected weight not equal to actual weight
+		this.system.setCustomerNoBag(true);
+		this.system.setAttendantApproval(true);
+		this.station.baggingArea.add(unit1);
+		
+		//check if station is disabled
+		assertEquals(true, station.handheldScanner.isDisabled());
+		assertEquals(true, station.mainScanner.isDisabled());
+		assertEquals(true, station.billInput.isDisabled());
+		
+		assertEquals(false, system.getUnApprovedDiscrepancy());
+		assertEquals(true,system.getDoNotBagItemActive());
+		assertEquals(true, system.getDiscrepancyActive());
+	}
+	
 }
